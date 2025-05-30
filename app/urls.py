bbaa97f0bf506b184f34426dff91ddf45a7b5b28
i-sestrem <nicolas.sestrem@gmail.com>
2025-05-29 23:13:21 -0300

    feat: inclusão da app de upload e associação de imagens

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,9 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
 )
 from rest_framework.routers import DefaultRouter
+from django.conf import settings
+from django.conf.urls.static import static
+from uploader.router import router as uploader_router
 
 from core.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, LivroViewSet, UserViewSet
 
@@ -31,6 +34,9 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name='schema'),
         name='redoc',
     ),
+    path("api/media/", include(uploader_router.urls)),
     # API
     path('api/', include(router.urls)),
 ]
+
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
